1fe9ce4d9eabb64733633fba1c0c5fc1d42273f1
Sergey Gladyshev <sogladyshev@avito.ru>
2017-07-20 11:12:20 +0300

    fix

--- handlers/bar.go
+++ handlers/bar.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+	"encoding/json"
 )
 
 type BarHandler struct {
@@ -13,5 +14,11 @@ func NewBarHandler() *BarHandler {
 
 func (h *BarHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	rw.WriteHeader(200)
-	rw.Write([]byte(`{"result":"ok"}`))
+
+	type Response struct {
+		Result string `json:"result"`
+	}
+
+	b, _ := json.Marshal(Response{Result:"ok"})
+	rw.Write(b)
 }
