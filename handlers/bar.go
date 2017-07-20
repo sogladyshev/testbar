f8be1c46e4e9504ddbcc55ba33f186bfa1069865
Sergey Gladyshev <sogladyshev@avito.ru>
2017-07-20 10:55:51 +0300

    fix

--- handlers/bar.go
+++ handlers/bar.go
@@ -2,9 +2,6 @@ package handlers
 
 import (
 	"net/http"
-	"io/ioutil"
-	"encoding/json"
-	"log"
 )
 
 type BarHandler struct {
@@ -14,20 +11,7 @@ func NewBarHandler() *BarHandler {
 	return &BarHandler{}
 }
 
-type RequestBar struct {
-	Result string `json:"result"`
-}
-
 func (h *BarHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	defer req.Body.Close()
-	reqBodyBytes, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		log.Println("error in read request" + err.Error())
-		rw.Write([]byte(err.Error()))
-	}
-	var reqBody *RequestBar
-	err = json.Unmarshal(reqBodyBytes, &reqBody)
-
 	rw.WriteHeader(200)
-	rw.Write([]byte(reqBody.Result))
+	rw.Write([]byte(`{"result":"ok"}`))
 }
